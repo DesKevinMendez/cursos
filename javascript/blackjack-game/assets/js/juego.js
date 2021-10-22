7ac15888b810f004536b126926d52e8b986eebf3
Kevin <kevin_mendez30@outlook.com>
2021-10-21 22:23:05 -0600

    se agrega código para pedir y generar cargas

--- javascript/blackjack-game/assets/js/juego.js
+++ javascript/blackjack-game/assets/js/juego.js
@@ -1,21 +1,35 @@
-let check = []
+let deck = []
 const tipos = ['C', 'D', 'H', 'S']
 const especiales = ['A', 'J', 'Q', 'K']
 
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (const tipo of tipos) {
-      check.push(i+tipo)
+      deck.push(i+tipo)
     }
   }
 
   for (const tipo of tipos) {
     for (const esp of especiales) {
-      check.push(esp+tipo)
+      deck.push(esp+tipo)
     }
   }
-  check = _.shuffle(check)
-  console.log(check);
+  return _.shuffle(deck)
 }
 
 crearDeck()
+
+const perdirCarta = ()=>{
+  if (deck.length === 0) {
+    throw 'No hay más cartas en el deck'
+  }
+  return crearDeck().pop()
+}
+
+const valorCarta = (carta)=> {
+  const valor = carta.substring(0, carta.length - 1)
+  return (isNaN(valor)) ? 
+    (valor === 'A') ? 11 : 10
+    : parseInt(valor)
+}
+console.log(valorCarta(perdirCarta()))
