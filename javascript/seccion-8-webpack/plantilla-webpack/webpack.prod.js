92b583f66d7f3784817e6fbb3a8dd1e155560ff9
Kevin <kevin_mendez30@outlook.com>
2021-11-15 21:38:38 -0600

    feat: create file for webpack production

--- javascript/seccion-8-webpack/plantilla-webpack/webpack.prod.js
+++ javascript/seccion-8-webpack/plantilla-webpack/webpack.prod.js
@@ -1,10 +1,12 @@
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  output: { clean: false }, // regenera el build
+  mode: 'production',
+  output: { clean: true, filename: 'main.[fullhash].js' }, // regenera el build
   module: {
     rules: [
       {
@@ -29,13 +31,20 @@ module.exports = {
       }
     ],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ],
+  },
   plugins: [
     new htmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: '[name].[fullhash].css',
       ignoreOrder: false,
     }),
     new CopyWebpackPlugin(
