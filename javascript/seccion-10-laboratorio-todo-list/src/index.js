1c0c4a1880429a2e52c9717fa8f4fa33e9ff016f
Kevin <kevin_mendez30@outlook.com>
2021-11-17 19:24:39 -0600

    render li with the first todo

--- javascript/seccion-10-laboratorio-todo-list/src/index.js
+++ javascript/seccion-10-laboratorio-todo-list/src/index.js
@@ -1,7 +1,10 @@
 import './styles.css'
 import { Todo, TodoList } from './classes'
+import { crearTodoHTML } from './js/components';
 
 const todoList = new TodoList();
 const tarea = new Todo('Aprender javascript')
 
 todoList.nuevoTodo(tarea)
+
+crearTodoHTML(tarea)
