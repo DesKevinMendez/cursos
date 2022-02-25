692330b5d053ae904c606d5797edd1291cd24a67
Kevin <kevin_mendez30@outlook.com>
2022-02-24 22:14:01 -0600

    setup

--- Vue-3 fernando-herrera/introduction/app.js
+++ Vue-3 fernando-herrera/introduction/app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
-  data: ()=> ({
-    value: 'Desde app.js'
-  })
+  // data: ()=> ({
+  //   value: 'Desde app.js'
+  // }),
+  setup: ()=> {
+    value = Vue.ref('data')
+    return { value }
+  }
 })
 
 app.mount('#app')
