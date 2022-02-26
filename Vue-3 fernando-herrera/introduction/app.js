5d4f387f5069b53e7dfb5d537f56fb2f05282d4b
Kevin <kevin_mendez30@outlook.com>
2022-02-25 22:16:15 -0600

    added new values with target.value

--- Vue-3 fernando-herrera/introduction/app.js
+++ Vue-3 fernando-herrera/introduction/app.js
@@ -1,19 +1,28 @@
 const app = Vue.createApp({
-  setup: ()=> {
-    value = Vue.ref('data')
+  setup: () => {
+    value = Vue.ref('data');
+    newValue = Vue.ref('');
     vFor = Vue.reactive([
-      {id: 1, name: 'Kevin'},
-      {id: 2, name: 'Ezequiel'}
-    ])
-    capitalize = ()=> {
-      value.value = value.value.toUpperCase()
-    }
+      { id: 1, name: 'Kevin' },
+      { id: 2, name: 'Ezequiel' }
+    ]);
+    capitalize = () => {
+      value.value = value.value.toUpperCase();
+    };
+    changeValue = (e) => {
+      const newVFor = {
+        id: vFor.length + 1,
+        name: e.target.value
+      };
+      vFor.unshift(newVFor);
+      newValue.value = ''
+    };
     change = (e) => {
-      value.value = 'new change'
+      value.value = 'new change';
       capitalize();
-    }
-    return { value, change, vFor }
+    };
+    return { value, change, vFor, changeValue, newValue };
   }
-})
+});
 
-app.mount('#app')
+app.mount('#app');
